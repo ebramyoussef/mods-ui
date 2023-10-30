0561986b83ecb89d93f3c80b5d1e3bbacfbb748f
ebramyoussef <ebramnimr2016@gmail.com>
2023-10-30 14:32:31 -0700

    Create test_screen.py

---
+++ mods_ui/test_screen.py
@@ -0,0 +1,28 @@
+from os import path
+from pydm import Display
+from PyQt5.QtWidgets import QApplication
+import sys
+
+
+class testScreen(Display):
+    def __init__(self, parent=None, args=None, macros=None):
+        super(testScreen, self).__init__(
+            parent=parent, args=args, macros=macros
+        )
+        self.show()
+
+    def ui_filename(self):
+        return "untitled.ui"
+
+    def ui_filepath(self):
+        return path.join(
+            path.dirname(path.realpath(__file__)), self.ui_filename()
+        )
+
+
+if __name__ == "__main__":
+    qapp = QApplication.instance()
+    if not qapp:
+        qapp = QApplication(sys.argv)
+    screen = testScreen()
+    qapp.exec_()
