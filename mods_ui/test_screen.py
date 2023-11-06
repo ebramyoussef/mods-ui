3edb81227a0980a66d15b8f75ff480f98e4e340b
ebramyoussef <ebramnimr2016@gmail.com>
2023-11-06 10:49:20 -0800

    Update test_screen.py

--- mods_ui/test_screen.py
+++ mods_ui/test_screen.py
@@ -33,10 +33,10 @@ class testScreen(Display):
         self.FF_cam = CamIOC(
             base_pv="LM1K4:COM_DP1_TF1_FF1:", classification="FF"
         )
-        self.ui.NFImageView.imageChannel(self.NF_cam.image_ca)
-        self.ui.NFImageView.widthChannel(self.NF_cam.width_ca)
-        self.ui.FFImageView.imageChannel(self.FF_cam.image_ca)
-        self.ui.FFImageView.widthChannel(self.FF_cam.width_ca)
+        self.ui.NFImageView.imageChannel(value=self.NF_cam.image_ca)
+        self.ui.NFImageView.widthChannel(value=self.NF_cam.width_ca)
+        self.ui.FFImageView.imageChannel(value=self.FF_cam.image_ca)
+        self.ui.FFImageView.widthChannel(value=self.FF_cam.width_ca)
         self.ui.NFImageSettingsPushButton.clicked.connect(
             lambda: self.load_image_settings(self.ui.NFImageView)
         )
