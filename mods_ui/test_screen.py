7d4457ea56f9bb960d1ffa57c5ee72846b1a6365
ebramyoussef <ebramnimr2016@gmail.com>
2023-11-07 11:03:59 -0800

    Update test_screen.py

--- mods_ui/test_screen.py
+++ mods_ui/test_screen.py
@@ -54,7 +54,9 @@ class testScreen(pydm.Display):
         self.qss_file = "styles.qss"
         with open(self.qss_file, "r") as fh:
             self.setStyleSheet(fh.read())
-        self.resize(1200, 900)
+        # self.resize(1200, 900)
+        self.ui.NFRefGroupBox.setVisible(False)
+        self.ui.FFRefGroupBox.setVisible(False)
         self.ui.NFImageView.imageChannel = self.NF_cam.image_ca
         self.ui.FFImageView.imageChannel = self.FF_cam.image_ca
         self.ui.NFImageView.widthChannel = self.NF_cam.width_ca
