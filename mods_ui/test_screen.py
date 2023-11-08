80b7a958d7bc7a60cdc740d93f347fff23cc4a6c
ebramyoussef <ebramnimr2016@gmail.com>
2023-11-07 21:15:59 -0800

    Update test_screen.py

--- mods_ui/test_screen.py
+++ mods_ui/test_screen.py
@@ -23,7 +23,7 @@ class CamIOC:
         centroidy_pv_suffix="Stats2:CentroidY_RBV",
         sigmax_pv_suffix="Stats2:SigmaX_RBV",
         sigmay_pv_suffix="Stats2:SigmaY_RBV",
-        ellipse_overlay_pv_suffix="IMAGE1:ROI1:",
+        ellipse_overlay_pv_suffix="ROI1:",
     ):
         self.base_pv = base_pv
         self.classification = classification
